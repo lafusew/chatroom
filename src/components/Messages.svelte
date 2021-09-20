82c9579336662a63c5381042a88091dd9d7e8dec
LaFusew <antoine.oddoz@gmail.com>
2021-09-21 01:20:04 +0200

    - message list prototype added

---
+++ src/components/Messages.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  // List of all raw messages
+  export let messages: string[];
+</script>
+
+<div>
+  {#each messages as message}
+    <p>{message}</p>  
+  {/each}
+</div>
+
+<style>
+  div {
+    width: 300px;
+    height: 500px;
+    overflow-y: scroll;
+  }
+</style>
