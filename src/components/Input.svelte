47ecb1cd9f02946abaf2887bf0a93ce4c7dd2ea4
LaFusew <antoine.oddoz@gmail.com>
2021-09-21 01:19:50 +0200

    - Emitter input added

---
+++ src/components/Input.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  // Emiting method provided by App.Svelte
+  export let onEmit:(msg: string) => void;
+
+  //input ref & "controlled" value
+  let input: HTMLInputElement;
+  let inputValue: string = '';
+
+  // Trigger emit method and clear input
+  function handleClick() {
+    onEmit(inputValue);
+    inputValue = '';
+    input.focus();
+  }
+</script>
+
+<input bind:this={input} bind:value={inputValue}>
+<btn on:click={handleClick}>Emit</btn>
+<p>{inputValue}</p>
